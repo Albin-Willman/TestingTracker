ae2149c26cd0ff853c1ae598515b3b748384068d
Albin Svensson <albin.svensson@trialbee.com>
2015-03-15 23:26:12 +0100

    Allow creation of first user

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ The aim is to give a tool to small teams where not everyone has a github account
 
 ## Usage
 
-The first User has to be setup from the console. After that all users can create more users. There are no ownerships and no limits for any users (That's why it only is for small teams)
+If there is no users in the database any attempt to login will create a user and log you in.
+
+After that all users can create more users. There are no ownerships and no limits for any users (That's why it only is for small teams)
 
 If you setup github access tokens issues reported in this tool will be created as github issues.
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,10 +2,12 @@ class UserSessionsController < ApplicationController
 
   def new
     @user_session = UserSession.new
+    @no_users = User.any? == false
   end
 
   # Handles log in request
   def create
+    User.create(email: params[:user_session][:email], password: params[:user_session][:password]) unless User.any?
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       redirect_to user_url
@@ -16,7 +18,7 @@ class UserSessionsController < ApplicationController
 
   # Handles log out request
   def destroy
-    current_user_session.destroy
+    current_user_session.try(:destroy)
     redirect_to root_url
   end
 
