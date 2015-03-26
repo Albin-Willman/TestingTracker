7328f302aeb54c6a3b0bacc1fc8bc72c26270016
Albin Svensson <albin.svensson@trialbee.com>
2015-03-27 00:50:26 +0100

    test organization repos

--- spec/models/github_token_spec.rb
+++ spec/models/github_token_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe GithubToken, type: :model do
     expect(t.errors.any?).to be_truthy
   end
 
+  context 'repos' do
+    let (:personal_repos) {
+      'personal repos'
+    }
+    let (:org_repos) {
+      "repos for org"
+    }
+    before(:each) do
+      allow_any_instance_of(Octokit::Client).to receive(:repos).and_return(personal_repos)
+      allow_any_instance_of(Octokit::Client).to receive(:org_repos).and_return(org_repos)
+    end
+
+    it 'return repos for github user if no organization is set' do
+      token = GithubToken.new(title: 'Test', access_token: 'aasdasd')
+      expect(token.repos).to eq(personal_repos)
+    end
+
+    it 'return org repos if org is set' do
+      token = GithubToken.new(title: 'Test', access_token: 'aasdasd', organization: 'org')
+      expect(token.repos).to eq(org_repos)
+    end
+  end
+
+
+  
+
 end
