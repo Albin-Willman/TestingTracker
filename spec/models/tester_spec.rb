380efc58d253afe4289ed2d1102b150aca0a9a47
Albin Svensson <albin.svensson@trialbee.com>
2015-03-28 10:38:54 +0100

    Wrote tests for testers

--- spec/models/tester_spec.rb
+++ spec/models/tester_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Tester, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    PRESENCE_ERROR = "can't be blank"
+    it 'validates the presence of a user' do
+      tester = Tester.create
+      expect(tester.errors.any?).to be_truthy
+      expect(tester.errors.messages[:user]).to include(PRESENCE_ERROR)
+    end
+
+    it 'validates the presence of a test_suite' do
+      tester = Tester.create
+      expect(tester.errors.any?).to be_truthy
+      expect(tester.errors.messages[:test_suite]).to include(PRESENCE_ERROR)
+    end
+
+    it 'can be created without errors' do
+      user       = User.create! email: 'email@example.com', password: '123456'
+      test_suite = TestSuite.create! name: 'test', status: 'active'
+      tester = Tester.create user: user, test_suite: test_suite
+      expect(tester.errors.any?).to be_falsey
+    end
+  end
 end
