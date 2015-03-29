4e414688af9f0081d96b7a58c62de2227ddeee3f
Albin Svensson <albin.svensson@trialbee.com>
2015-03-29 20:10:11 +0200

    Added tests for test suites

--- spec/models/test_suite_spec.rb
+++ spec/models/test_suite_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TestSuite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    it 'validates the presence of a name' do
+      test_suite = TestSuite.create
+      expect(test_suite.errors.any?).to be_truthy
+      expect(test_suite.errors.messages[:name]).to include(PRESENCE_ERROR)
+    end
+
+    it 'validates the presence of a status' do
+      test_suite = TestSuite.create
+      expect(test_suite.errors.any?).to be_truthy
+      expect(test_suite.errors.messages[:status]).to include(PRESENCE_ERROR)
+    end
+
+    it 'can be created without errors' do
+      test_suite = TestSuite.create name: 'Name', status: 'active'
+      expect(test_suite.errors.any?).to be_falsey
+    end
+  end
 end
