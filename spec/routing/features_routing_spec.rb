e0485de80d5382851a5ef077d6ea9d3e8d1eae04
Albin Svensson <albin.svensson@trialbee.com>
2015-03-31 06:57:57 +0200

    Changed layout a little bit

--- spec/routing/features_routing_spec.rb
+++ spec/routing/features_routing_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe FeaturesController, type: :routing do
   describe "routing" do
-
     it "routes to #index" do
       expect(:get => "test_suites/1/features").to route_to("features#index", test_suite_id: "1")
     end
@@ -31,5 +30,8 @@ RSpec.describe FeaturesController, type: :routing do
       expect(:delete => "test_suites/1/features/1").to route_to("features#destroy", :id => "1", test_suite_id: "1")
     end
 
+    it "routes to #approve" do
+      expect(:post => "test_suites/1/features/1/approve").to route_to("features#approve", :id => "1", test_suite_id: "1")
+    end
   end
 end
