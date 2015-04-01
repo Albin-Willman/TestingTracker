d897daa94bdd3d958ba19bd6b02e0ae0c5cccfd8
Albin Svensson <albin.svensson@trialbee.com>
2015-04-02 01:25:06 +0200

    Better testability

---
+++ app/helper_classes/table_builder.rb
@@ -0,0 +1,53 @@
+class TableBuilder
+  include ActionView::Helpers::TagHelper
+  include ActionView::Context
+  def initialize(opts = {})
+    @opts = opts
+  end
+
+  def generate
+    content_tag :table, class: table_class do
+      safe_join([
+        table_header,
+        table_body
+      ])
+    end
+  end
+
+  private
+
+  def table_header
+    return unless header = @opts[:header]
+    content_tag :thead do
+      table_row(true, header)
+    end
+  end
+
+  def table_body
+    return unless rows = @opts[:content]
+    content_tag :tbody do
+      safe_join(rows.map { |row| table_row(false, row) })
+    end
+  end
+
+  def table_row(header, elements)
+    return unless elements
+    content_tag :tr do
+      safe_join(elements.map { |e| table_cell(header, e) })
+    end
+  end
+
+  def table_cell(header, element)
+    tag_type = header ? :th : :td
+    content_tag tag_type do
+      element.to_s
+    end
+  end
+
+  def table_class
+    classes = ['table']
+    classes << 'table-striped' if @opts[:striped]
+    classes << 'js-datatable'  if @opts[:data_table]
+    classes.join(' ')
+  end
+end
---
+++ spec/helper_classes/table_builder_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe TableBuilder, type: :model do
+
+  let (:header_opts) { { header: ['test1', 'test2', 'test3'] } }
+  let (:header_html) { "<thead><tr><th>test1</th><th>test2</th><th>test3</th></tr></thead>" }
+  let (:content_opts) { { content: [[1,2,3],[2,3,5],[3,5,8]] } }
+  let (:content_html) { "<tbody><tr><td>1</td><td>2</td><td>3</td></tr><tr><td>2</td><td>3</td><td>5</td></tr><tr><td>3</td><td>5</td><td>8</td></tr></tbody>" }
+  let (:table_builder) { TableBuilder }
+
+  it 'generates headers for tables' do
+    opts = header_opts
+    expect(table_builder.new(opts).generate).to eq("<table class=\"table\">#{header_html}</table>")
+  end
+
+  it 'generates bodies for tables' do
+    opts = content_opts
+    expect(table_builder.new(opts).generate).to eq("<table class=\"table\">#{content_html}</table>")
+  end
+
+  it 'generates striped tables' do
+    opts = {
+        striped: true
+      }.merge(content_opts)
+    expect(table_builder.new(opts).generate).to eq("<table class=\"table table-striped\">#{content_html}</table>")
+  end
+
+  it 'generates datatables' do
+    opts = {
+        data_table: true
+      }.merge(content_opts)
+    expect(table_builder.new(opts).generate).to eq("<table class=\"table js-datatable\">#{content_html}</table>")
+  end
+
+end
+
